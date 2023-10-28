bceb41318f9ba3cb7b0e2ba0e2368ed4506302a3
Mastouramsa <118532234+Mastouramsa@users.noreply.github.com>
2023-10-28 22:43:46 +0200

    Update database.py
    Ajoute un commentaire

--- server/database.py
+++ server/database.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 engine = create_engine("sqlite:///../chinook.db", echo=True)
-
+## Creates a session to interact with the database.
 session = Session(bind=engine)
